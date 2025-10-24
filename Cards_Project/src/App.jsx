e3c6b98ce0f392efc1531ed0688366418ce912b7
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-24 10:26:16 +0500

    Update App.jsx

--- Cards_Project/src/App.jsx
+++ Cards_Project/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import UserCard from "../../components/UserCard";
-import Data from "../../components/user.json"
+import UserCard from "../../../components/UserCard";
+import Data from "../../../components/user.json";
 
 const App = () => {
-  
-   
   return (
     <div className="grid grid-cols-3 gap-8 max-[1200px]:grid-cols-3  max-[900px]:grid-cols-2 max-[600px]:grid-cols-1 p-2 ">
       {Data.map((item, index) => {
         return (
-          <UserCard Name={item.Name} key={index} role={item.role} Image={item.Image} />
+          <UserCard
+            Name={item.Name}
+            key={index}
+            role={item.role}
+            Image={item.Image}
+          />
         );
       })}
     </div>
