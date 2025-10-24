d4d86d4de76d6c3c9c27fe72d9fb567bb094d8d7
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-24 10:25:20 +0500

    Add Project Folder

---
+++ Cards_Project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UserCard from "../../components/UserCard";
+import Data from "../../components/user.json"
+
+const App = () => {
+  
+   
+  return (
+    <div className="grid grid-cols-3 gap-8 max-[1200px]:grid-cols-3  max-[900px]:grid-cols-2 max-[600px]:grid-cols-1 p-2 ">
+      {Data.map((item, index) => {
+        return (
+          <UserCard Name={item.Name} key={index} role={item.role} Image={item.Image} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
